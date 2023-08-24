818a9a509135487ecdc00443099d79dbfa8e7513
Oleksandr Khomiak <khomiak@gmail.com>
2023-08-24 18:33:45 +0300

    registration form

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,20 +1,94 @@
-import { FormContainer } from './RegisterForm.styled';
+import React from 'react';
 
+// Стилізовані компоненти
+import {
+  FormContainer,
+  FormTitle,
+  InputContainer,
+  Label,
+  Input,
+  Button,
+} from './RegisterForm.styled';
+
+// Бібліотека формік
+import { Formik, Form, ErrorMessage } from 'formik';
+
+// Бібліотека валідації форми yup
+import * as yup from 'yup';
+
+// Схма валідації форми!!!!!!
+// ПЕРЕВІРИТИ ШОБ ЗБІГАЛАСЬ З БЕКЕНДОМ
+// https://www.npmjs.com/package/yup
+const schema = yup.object().shape({
+  name: yup.string().required().min(4).max(30),
+  email: yup.string().email().required(),
+  password: yup.string().required().min(8),
+});
+
+//  Компонент форми
 const RegisterForm = () => {
-  const onSubmitForm = e => {};
+  // початкові значення полів форми
+  const initialValues = { name: '', email: '', password: '' };
+
+  // Обробник сабміту
+  const handleSubmit = (values, actions) => {
+    // у обʼєкт values повертаються дані з форми
+    console.log('values', values);
+    // тут буде проходити реєстрація
+    actions.resetForm();
+  };
 
   return (
     <FormContainer>
-      <form onSubmit={onSubmitForm}>
-        <h2>Sign Up</h2>
-        Name:
-        <input type="name" name="name" required />
-        Email:
-        <input type="email" name="email" required />
-        Password:
-        <input type="password" name="password" required />
-        <button type="submit">Sign Up</button>
-      </form>
+      <FormTitle>Sign Up</FormTitle>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={schema}
+      >
+        <Form autoComplete="off">
+          <InputContainer>
+            <Label htmlFor="name">Name</Label>
+            <Input type="text" name="name" placeholder="Enter your name" />
+            <ErrorMessage name="name" />
+          </InputContainer>
+
+          <InputContainer>
+            <Label htmlFor="email">Email </Label>
+            <Input type="text" name="email" placeholder="Enter email" />
+            <ErrorMessage name="email" />
+          </InputContainer>
+
+          <InputContainer>
+            <Label htmlFor="name">Password </Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Enter password"
+            />
+            <ErrorMessage name="password" />
+          </InputContainer>
+
+          <Button type="submit">
+            Sign Up
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+            >
+              <path
+                d="M12.5 2.5H13.5C14.9001 2.5 15.6002 2.5 16.135 2.77248C16.6054 3.01217 16.9878 3.39462 17.2275 3.86502C17.5 4.3998 17.5 5.09987 17.5 6.5V13.5C17.5 14.9001 17.5 15.6002 17.2275 16.135C16.9878 16.6054 16.6054 16.9878 16.135 17.2275C15.6002 17.5 14.9001 17.5 13.5 17.5H12.5M8.33333 5.83333L12.5 10M12.5 10L8.33333 14.1667M12.5 10L2.5 10"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </Button>
+        </Form>
+      </Formik>
     </FormContainer>
   );
 };
--- src/components/SignUp/SignUp.styled.jsx
+++ src/components/SignUp/SignUp.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const MainStyled = styled.main`
   display: flex;
@@ -8,4 +9,21 @@ export const MainStyled = styled.main`
   height: 100vh;
 
   background-color: #dcebf7;
+`;
+
+export const StyledLink = styled(Link)`
+  color: #3e85f3;
+  text-shadow: 0px 9.399999618530273px 57.6875px rgba(0, 0, 0, 0.04),
+    0px 47px 355px rgba(0, 0, 0, 0.07);
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.16667;
+  text-decoration-line: underline;
+
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+    line-height: 1.33333;
+  }
 `;
