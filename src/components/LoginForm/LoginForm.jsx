90433db582c46e25fe289e4c963d304cb2ec26ea
Oleksandr Khomiak <khomiak@gmail.com>
2023-08-26 21:01:17 +0300

    login authorization

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 // import { useDispatch } from 'react-redux';
 // Стилізовані компоненти
@@ -10,26 +9,36 @@ import {
   Label,
   Field,
   Button,
-  TextShow
+  TextShow,
 } from './LoginForm.styled';
 
 // Бібліотека формік
-import { Formik, Form,  } from 'formik';
+import { Formik, Form } from 'formik';
 
 // Бібліотека валідації форми yup
 import * as yup from 'yup';
+import { useDispatch } from 'react-redux';
+import authOperations from 'redux/auth/authOperations';
 
 // Схма валідації форми!!!!!!
 // ПЕРЕВІРИТИ ШОБ ЗБІГАЛАСЬ З БЕКЕНДОМ
 // https://www.npmjs.com/package/yup
 const schema = yup.object().shape({
-  email: yup.string()
+  email: yup
+    .string()
     .email()
-    .matches(/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/, 'Email must contain only Latin characters')
+    .matches(
+      /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+      'Email must contain only Latin characters'
+    )
     .required(),
-  password: yup.string()
+  password: yup
+    .string()
     .required()
-    .matches(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 'Password requires min 8 characters long & UPPERCASE & lowercase letter')
+    .matches(
+      /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+      'Password requires min 8 characters long & UPPERCASE & lowercase letter'
+    )
     .min(8),
 });
 
@@ -37,6 +46,7 @@ const schema = yup.object().shape({
 const LoginForm = () => {
   // початкові значення полів форми
   const [passwordShown, setPasswordShown] = useState(false);
+  const dispatch = useDispatch();
 
   const togglePassword = () => {
     // When the handler is invoked
@@ -46,66 +56,62 @@ const LoginForm = () => {
 
   // Обробник сабміту
   const handleSubmit = (values, actions) => {
+    dispatch(authOperations.logIn(values));
     // у обʼєкт values повертаються дані з форми
     console.log('values', values);
     // тут буде проходити реєстрація
-    actions.resetForm();
+    // actions.resetForm();
   };
 
   return (
     <FormContainer>
       <FormTitle>Log In</FormTitle>
       <Formik
-      initialValues={{ email: '', password: '' }}
+        initialValues={{ email: '', password: '' }}
         onSubmit={handleSubmit}
         validationSchema={schema}
       >
         {() => (
-        <Form autoComplete="off">
-          <InputContainer>
-            <Label htmlFor="email">Email </Label>
-            <Field 
-            type="text" 
-            name="email" 
-            placeholder="Enter email" />
-            <ErrorMessage 
-            name="email" 
-            component="div"/>
-          </InputContainer>
+          <Form autoComplete="off">
+            <InputContainer>
+              <Label htmlFor="email">Email </Label>
+              <Field type="text" name="email" placeholder="Enter email" />
+              <ErrorMessage name="email" component="div" />
+            </InputContainer>
 
-          <InputContainer>
-            <Label htmlFor="name">Password </Label>
-            <Field
-              // type="password"
-              name="password"
-              placeholder="Enter password"
-              type={passwordShown ? "text" : "password"}
-            />
-            <TextShow 
-            type="submit"
-            onClick={togglePassword}>Show</TextShow>
-            <ErrorMessage name="password" component="div"/>
-          </InputContainer>
-
-          <Button type="submit">
-              Log In
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-            >
-              <path
-                d="M12.5 2.5H13.5C14.9001 2.5 15.6002 2.5 16.135 2.77248C16.6054 3.01217 16.9878 3.39462 17.2275 3.86502C17.5 4.3998 17.5 5.09987 17.5 6.5V13.5C17.5 14.9001 17.5 15.6002 17.2275 16.135C16.9878 16.6054 16.6054 16.9878 16.135 17.2275C15.6002 17.5 14.9001 17.5 13.5 17.5H12.5M8.33333 5.83333L12.5 10M12.5 10L8.33333 14.1667M12.5 10L2.5 10"
-                stroke="white"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+            <InputContainer>
+              <Label htmlFor="name">Password </Label>
+              <Field
+                // type="password"
+                name="password"
+                placeholder="Enter password"
+                type={passwordShown ? 'text' : 'password'}
               />
-            </svg>
-          </Button>
-        </Form>
+              <TextShow type="submit" onClick={togglePassword}>
+                Show
+              </TextShow>
+              <ErrorMessage name="password" component="div" />
+            </InputContainer>
+
+            <Button type="submit">
+              Log In
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+              >
+                <path
+                  d="M12.5 2.5H13.5C14.9001 2.5 15.6002 2.5 16.135 2.77248C16.6054 3.01217 16.9878 3.39462 17.2275 3.86502C17.5 4.3998 17.5 5.09987 17.5 6.5V13.5C17.5 14.9001 17.5 15.6002 17.2275 16.135C16.9878 16.6054 16.6054 16.9878 16.135 17.2275C15.6002 17.5 14.9001 17.5 13.5 17.5H12.5M8.33333 5.83333L12.5 10M12.5 10L8.33333 14.1667M12.5 10L2.5 10"
+                  stroke="white"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </Button>
+          </Form>
         )}
       </Formik>
     </FormContainer>
