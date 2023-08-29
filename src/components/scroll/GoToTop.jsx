e361fae387a86d4199c403c574afd1ede4d4f700
Roksolana96 <roxolana.busko@gmail.com>
2023-08-29 21:51:23 +0200

    GoToTop

---
+++ src/components/scroll/GoToTop.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { Wrapper } from './GoToTop.styled'
+import { FaArrowUp } from "react-icons/fa";
+
+const GoToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const goToBtn = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const listenToScroll = () => {
+    let heightToHidden = 20;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > heightToHidden) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
+
+  return (
+    <>
+    <Wrapper>
+      {isVisible && (
+        <div className="top-btn" onClick={goToBtn}>
+          <FaArrowUp className="top-btn--icon" />
+        </div>
+      )}
+    </Wrapper>
+    </>
+  );
+};
+
+
+export default GoToTop;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,10 +3,11 @@ import AuthSection from '../components/AuthSection/AuthSection';
 import Description from '../components/Description/Description';
 import ReviewsSlider from '../components/ReviewsSlider/ReviewsSlider';
 import Footer from 'components/Footer/Footer';
-
+import GoToTop from '../components/scroll/GoToTop'
 const Home = () => {
   return (
     <>
+      <GoToTop />
       <AuthSection />
       <Description />
       <ReviewsSlider />
