1760c5b8c474ec2c440784388316375dc7e639e9
Daniil Mikhnevich <114292729+Furcreator@users.noreply.github.com>
2023-08-31 19:00:58 +0200

    user-name auth page

--- src/components/SharedLayout/UserInfo/UserInfo.jsx
+++ src/components/SharedLayout/UserInfo/UserInfo.jsx
@@ -8,14 +8,18 @@ import {
 } from './UserInfo.styled';
 import ThemeIcoButton from '../../../images/svg/moon.svg';
 import UserAvatar from '../../../images/accountPage/default-profile-avatar.png';
+import { useSelector } from 'react-redux';
+
 const UserInfo = () => {
+  const user = useSelector(state => state.auth.user);
+
   return (
     <ProfInfoWrap>
       <FeedbackButton type="button">Feedback</FeedbackButton>
       <ThemeToggleButton type="button">
         <img src={ThemeIcoButton} alt="change theme" width="100%" />
       </ThemeToggleButton>
-      <NameField>Nadiia</NameField>
+      <NameField>{user.username}</NameField>
       <AvatarThumb>
         <Avatar src={UserAvatar} alt="user avatar" />
       </AvatarThumb>
