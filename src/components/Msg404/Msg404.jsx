499cb60eac6c4cd65f1c74760faa123b8436723f
Oleksandr Khomiak <khomiak@gmail.com>
2023-09-01 16:01:15 +0300

    edit error page adaptive images

--- src/components/Msg404/Msg404.jsx
+++ src/components/Msg404/Msg404.jsx
@@ -7,7 +7,14 @@ import {
   LinkToMain,
 } from './Msg404.styled';
 
-import ErrGooseImage from '../../images/NotFoundPage/mobile_error.png';
+import ErrGooseImageMobile from '../../images/NotFoundPage/mobile_error.png';
+import ErrGooseImageMobile_2x from '../../images/NotFoundPage/mobile_error@2x.png';
+
+import ErrGooseImageTablet from '../../images/NotFoundPage/tablet_error.png';
+import ErrGooseImageTablet_2x from '../../images/NotFoundPage/tablet_error@2x.png';
+
+import ErrGooseImageDesktop from '../../images/NotFoundPage/disktop_error.png';
+import ErrGooseImageDesktop_2x from '../../images/NotFoundPage/disktop_error@2x.png';
 
 const Msg404 = () => {
   return (
@@ -16,7 +23,27 @@ const Msg404 = () => {
         <MessageBlock>
           <ErrImgContainer>
             <Text4FromImg>4</Text4FromImg>
-            <img src={ErrGooseImage} alt="error page" />
+            <picture>
+              <source
+                srcSet={`${ErrGooseImageDesktop}    1x,
+                    ${ErrGooseImageDesktop_2x}  2x`}
+                media="(min-width: 1440px)"
+              />
+              <source
+                srcSet={`${ErrGooseImageMobile}    1x,
+                    ${ErrGooseImageMobile_2x}  2x`}
+                media="(max-width: 767px)"
+              />
+              <source
+                srcSet={`${ErrGooseImageTablet}    1x,
+                    ${ErrGooseImageTablet_2x}  2x`}
+                media="(min-width: 768px)"
+              />
+
+              <img src={ErrGooseImageMobile} alt="error page" />
+            </picture>
+
+            {/* <img src={ErrGooseImageMobile} alt="error page" /> */}
             <Text4FromImg>4</Text4FromImg>
           </ErrImgContainer>
 
