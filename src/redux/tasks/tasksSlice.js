4f3e55719514b10a10036a3e91c71000f4f6c687
Daniil Mikhnevich <114292729+Furcreator@users.noreply.github.com>
2023-09-02 21:12:10 +0200

    add tasks slicer

---
+++ src/redux/tasks/tasksSlice.js
@@ -0,0 +1,55 @@
+import {
+  addTask,
+  deleteTask,
+  fetchAllTasks,
+  updateTask,
+} from './tasksOperation';
+
+const { createSlice } = require('@reduxjs/toolkit');
+
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState: {
+    items: [],
+    error: null,
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchAllTasks.fulfilled, (state, { payload }) => {
+        state.items = payload;
+        state.error = null;
+      })
+      .addCase(fetchAllTasks.rejected, (state, { payload }) => {
+        state.items = [];
+        state.error = payload;
+      })
+      .addCase(addTask.fulfilled, (state, { payload }) => {
+        state.items.push(payload);
+        state.error = null;
+      })
+      .addCase(addTask.rejected, (state, { payload }) => {
+        state.error = payload;
+      })
+      .addCase(updateTask.fulfilled, (state, { payload }) => {
+        const updatedTaskIndex = state.items.findIndex(
+          task => task.id === payload.id
+        );
+        if (updatedTaskIndex !== -1) {
+          state.items[updatedTaskIndex] = payload;
+        }
+        state.error = null;
+      })
+      .addCase(updateTask.rejected, (state, { payload }) => {
+        state.error = payload;
+      })
+      .addCase(deleteTask.fulfilled, (state, { payload }) => {
+        state.items = state.items.filter(task => task.id !== payload);
+        state.error = null;
+      })
+      .addCase(deleteTask.rejected, (state, { payload }) => {
+        state.error = payload;
+      });
+  },
+});
+
+export const tasksReducer = tasksSlice.reducer;
