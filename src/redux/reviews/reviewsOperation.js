758bbceee8ea271ca32dc991e5dc8097edcdce06
Daniil Mikhnevich <114292729+Furcreator@users.noreply.github.com>
2023-09-02 21:29:26 +0200

    add all reviews slice

--- src/redux/reviews/reviewsOperation.js
+++ src/redux/reviews/reviewsOperation.js
@@ -15,3 +15,51 @@ export const fetchReviews = createAsyncThunk(
     }
   }
 );
+
+export const fetchReviewByOwner = createAsyncThunk(
+  'reviews/fetchByOwner',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/reviews/own');
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addReview = createAsyncThunk(
+  'reviews/add',
+  async (review, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('/reviews/own', review);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateReview = createAsyncThunk(
+  'reviews/update',
+  async ({ id, updatedReview }, { rejectWithValue }) => {
+    try {
+      const response = await axios.patch(`/reviews/own/${id}`, updatedReview);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteReview = createAsyncThunk(
+  'reviews/delete',
+  async (id, { rejectWithValue }) => {
+    try {
+      await axios.delete(`/reviews/own/${id}`);
+      return id;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
--- src/redux/reviews/reviewsSelectors.js
+++ src/redux/reviews/reviewsSelectors.js
@@ -1,1 +1,2 @@
 export const selectReviews = state => state.reviews.items;
+export const selectOwnerReviews = state => state.reviews.ownerReviews;
