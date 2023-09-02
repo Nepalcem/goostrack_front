4d7fc0fef0186d7630a88ffb8bc6be778a2a9e0d
Oleksandr Siukov <as-service2008@ukr.net>
2023-09-02 18:09:44 +0300

    work

--- src/components/Calendar/ChoosedDay/ChoosedDay.jsx
+++ src/components/Calendar/ChoosedDay/ChoosedDay.jsx
@@ -1,37 +1,8 @@
 import DayCalendarHead from '../DayCalendarHead/DayCalendarHead';
 import { useParams } from 'react-router-dom';
 
-import { format, eachDayOfInterval, isSunday, addDays } from 'date-fns'; //- для роботи з днями місяця
-
 export const ChoosedDay = () => {
   const { currentDay } = useParams();
-
-  const day = currentDay.slice(0, 2);
-
-  const year = currentDay.slice(4);
-
-  const month = currentDay.slice(2, 4);
-
-  const todayUSA = new Date(`${year}, ${month}, ${day}`);
-  const todayEuro = addDays(todayUSA, -1);
-
-  const today = !isSunday(todayUSA) ? todayUSA : todayEuro;
-
-  // // ✅ Get the first day of the current week (Sunday)
-  const firstDay = new Date(
-    today.setDate(today.getDate() - today.getDay() + 1)
-  );
-
-  // // ✅ Get the last day of the current week (Saturday)
-  const lastDay = new Date(today.setDate(today.getDate() - today.getDay() + 7));
-
-  const daysOfWeek = eachDayOfInterval({
-    start: firstDay,
-    end: lastDay,
-  });
-
-  const dayNumber = daysOfWeek.map(dayOfWeek => format(dayOfWeek, 'dd'));
-  console.log(dayNumber);
   return (
     <div>
       <DayCalendarHead />
@@ -45,13 +16,3 @@ export const ChoosedDay = () => {
 // 5. Компонент рендерить:
 //  - DayCalendarHead - дні тижня з датами, клік по дню з датою показує колонки з задачами за обраний день.
 //  - TasksColumnsList - блок з трьома колонками списків завданнь - TasksColumn (To do | In progress | Done). На мобільній та планшетній версії має горизонтальний скрол, якщо колонок більше ніж вміщає ширина екрану пристрою юзера."
-
-// import { format, eachDayOfInterval } from 'date-fns';
-// import { enUS } from 'date-fns/locale';
-
-// const daysOfWeek = eachDayOfInterval({ start: new Date(), end: new Date(new
-//                   Date().setDate(new Date().getDate() + 6)) });
-
-// const weekdays = daysOfWeek.map((day) => format(day, 'EEE', { locale: enUS }));
-
-// console.log(weekdays); // ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
--- src/components/Calendar/DayCalendarHead/DayCalendarHead.jsx
+++ src/components/Calendar/DayCalendarHead/DayCalendarHead.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { useResize } from 'hooks/useResize';
-
 import { SCREEN_TABLET, nameDaysMobile, nameDaysTablet } from 'utils/variables';
+import { format, eachDayOfInterval, isSunday, addDays } from 'date-fns'; //- для роботи з днями місяця
 
 import {
   DayComponent,
@@ -12,13 +13,36 @@ import {
 const DayCalendarHead = () => {
   const nameDays =
     useResize().width < SCREEN_TABLET ? nameDaysMobile : nameDaysTablet;
+  //
+  const { currentDay } = useParams();
+  const day = currentDay.slice(0, 2);
+  const year = currentDay.slice(4);
+  const month = currentDay.slice(2, 4);
+  const todayUSA = new Date(`${year}, ${month}, ${day}`);
+  const todayEuro = addDays(todayUSA, -1);
+  const today = !isSunday(todayUSA) ? todayUSA : todayEuro;
+  // // ✅ Get the first day of the current week (Sunday)
+  const firstDay = new Date(
+    today.setDate(today.getDate() - today.getDay() + 1)
+  );
+  // // ✅ Get the last day of the current week (Saturday)
+  const lastDay = new Date(today.setDate(today.getDate() - today.getDay() + 7));
+  const daysOfWeek = eachDayOfInterval({
+    start: firstDay,
+    end: lastDay,
+  });
+  // const dayNumber = daysOfWeek.map(dayOfWeek => format(dayOfWeek, 'dd'));
 
+  //
   return (
     <NameDaysComponent>
       {nameDays.map((nameDay, index) => (
         <DayComponent key={index} nameDay={nameDay}>
           {nameDay}
-          <DayNumber>{index}</DayNumber>
+          {/* <DayNumber key={daysOfWeek[index]}>{dayNumber[index]}</DayNumber> */}
+          <DayNumber key={daysOfWeek[index]}>
+            {format(daysOfWeek[index], 'dd')}
+          </DayNumber>
         </DayComponent>
       ))}
     </NameDaysComponent>
