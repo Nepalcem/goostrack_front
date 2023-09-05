bf9729dcc4e72afa8fed20360dc4296ad0ea788b
Daniil Mikhnevich <114292729+Furcreator@users.noreply.github.com>
2023-09-06 00:19:56 +0200

    update style on colum list

--- src/components/Calendar/TasksColumnsList/TasksColumnsList.styled.jsx
+++ src/components/Calendar/TasksColumnsList/TasksColumnsList.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { ReactSVG } from 'react-svg';
 
 export const MainTasksContainer = styled.div`
-  // overflow-x: scroll;
+  overflow-x: scroll;
   display: flex;
   gap: 20px;
   /* overflow: hidden; */
@@ -95,8 +95,6 @@ export const AddTaskButton = styled.button`
 
   cursor: pointer;
 
-
-
   @media screen and (min-width: 768px) {
     width: 24px;
     height: 24px;
@@ -170,10 +168,6 @@ export const AddTaskButtonBig = styled.button`
   font-style: normal;
   font-weight: 600;
   line-height: 1.28571;
-
-  &:hover {
-    background-color: #2b78ef;
-  }
 `;
 
 export const IcoAdd2 = styled(ReactSVG)`
