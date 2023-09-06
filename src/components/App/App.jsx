eae9ef3ad6bc5161acd5fd16fc477fe9a0452308
Oleksandr Khomiak <khomiak@gmail.com>
2023-09-06 16:35:02 +0300

    fixing-isFetchingCurrentUser

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,11 +8,11 @@ import { Loader } from 'components/Loader/Loader';
 //  приватні і рестріктед роуд
 import RestrictedRoute from '../RestrictedRoute/RestrictedRoute';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import authOperations from 'redux/auth/authOperations';
 
-import {useAuth} from 'hooks/useAuth' //loading
+import { useAuth } from 'hooks/useAuth'; //loading
 
 const MainPage = lazy(() => import('../../pages/MainPage'));
 const RegisterPage = lazy(() => import('../../pages/RegisterPage'));
@@ -20,7 +20,9 @@ const LoginPage = lazy(() => import('../../pages/LoginPage'));
 const NotFound = lazy(() => import('../../pages/NotFound'));
 const AccountPage = lazy(() => import('../../pages/AccountPage'));
 const StatisticsPage = lazy(() => import('../../pages/StatisticsPage'));
-const CalendarPage = lazy(() => import('../../pages/CalendarPage/CalendarPage'));
+const CalendarPage = lazy(() =>
+  import('../../pages/CalendarPage/CalendarPage')
+);
 
 // import MainPage from '../../pages/MainPage';
 // import RegisterPage from '../../pages/RegisterPage';
@@ -31,8 +33,6 @@ const CalendarPage = lazy(() => import('../../pages/CalendarPage/CalendarPage'))
 
 // import CalendarPage from '../../pages/CalendarPage/CalendarPage';
 
-
-
 export const App = () => {
   const dispatch = useDispatch();
   const { isRefreshing } = useAuth(); //loading
@@ -41,69 +41,83 @@ export const App = () => {
     dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
 
+  const isFetchingCurrentUser = useSelector(
+    state => state.auth.isFetchingCurrentUser
+  );
+  console.log('isFetchingCurrentUser', isFetchingCurrentUser);
+
   return (
     <>
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        {/* <Route path="/" element={<MainPage />} /> */}
-        <Route
-          path="/"
-          element={
-            <RestrictedRoute conponent={MainPage} redirectTo="/calendar" />
-          }
-        />
+      {!isFetchingCurrentUser && (
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            {/* <Route path="/" element={<MainPage />} /> */}
+            <Route
+              path="/"
+              element={
+                <RestrictedRoute conponent={MainPage} redirectTo="/calendar" />
+              }
+            />
 
-        <Route path="/" element={<SharedLayout />}>
-          {/* <Route path="/account" element={<AccountPage />} /> */}
-          <Route
-            path="/account"
-            element={
-              <PrivateRoute conponent={AccountPage} redirectTo="/login" />
-            }
-          />
-          {/* <Route path="/statistics" element={<StatisticsPage />} /> */}
-          <Route
-            path="/statistics"
-            element={
-              <PrivateRoute conponent={StatisticsPage} redirectTo="/login" />
-            }
-          />
-          {/* <Route path="/calendar" element={<CalendarPage />}></Route> */}
-          <Route
-            path="/calendar"
-            element={
-              <PrivateRoute conponent={CalendarPage} redirectTo="/login" />
-            }
-          >
-            <Route path="month/:currentDate" element={<ChoosedMonth />} />
-            <Route path="day/:currentDay" element={<ChoosedDay />} />
+            <Route path="/" element={<SharedLayout />}>
+              {/* <Route path="/account" element={<AccountPage />} /> */}
+              <Route
+                path="/account"
+                element={
+                  <PrivateRoute conponent={AccountPage} redirectTo="/login" />
+                }
+              />
+              {/* <Route path="/statistics" element={<StatisticsPage />} /> */}
+              <Route
+                path="/statistics"
+                element={
+                  <PrivateRoute
+                    conponent={StatisticsPage}
+                    redirectTo="/login"
+                  />
+                }
+              />
+              {/* <Route path="/calendar" element={<CalendarPage />}></Route> */}
+              <Route
+                path="/calendar"
+                element={
+                  <PrivateRoute conponent={CalendarPage} redirectTo="/login" />
+                }
+              >
+                <Route path="month/:currentDate" element={<ChoosedMonth />} />
+                <Route path="day/:currentDay" element={<ChoosedDay />} />
 
-            {/* <Route path="Month" element={<ChoosedMonth />} /> */}
-            {/* <Route path="movies/:movieId" element={<MovieDetails />}> */}
-            {/* <Route path="reviews" element={<Reviews />} /> */}
-          </Route>
-        </Route>
+                {/* <Route path="Month" element={<ChoosedMonth />} /> */}
+                {/* <Route path="movies/:movieId" element={<MovieDetails />}> */}
+                {/* <Route path="reviews" element={<Reviews />} /> */}
+              </Route>
+            </Route>
 
-        {/* <Route path="/register" element={<RegisterPage />} /> */}
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute conponent={RegisterPage} redirectTo="/calendar" />
-          }
-        />
+            {/* <Route path="/register" element={<RegisterPage />} /> */}
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  conponent={RegisterPage}
+                  redirectTo="/calendar"
+                />
+              }
+            />
 
-        {/* <Route path="/login" element={<LoginPage />} /> */}
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute conponent={LoginPage} redirectTo="/calendar" />
-          }
-        />
+            {/* <Route path="/login" element={<LoginPage />} /> */}
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute conponent={LoginPage} redirectTo="/calendar" />
+              }
+            />
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Suspense>
-    {isRefreshing && <Loader />}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      )}
+
+      {isRefreshing && <Loader />}
     </>
   );
 };
